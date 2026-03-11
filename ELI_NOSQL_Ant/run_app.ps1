8300bcd72c318fcf0347773bced89c85de8d2c88
M4nu3l202400653 <3022229520101@ingenieria.usac.edu.gt>
2026-03-11 14:09:35 -0600

    ultimos cambios codigo

--- ELI_NOSQL_Ant/run_app.ps1
+++
@@ -1,7 +0,0 @@
-Set-Location 'C:\Users\octav\Documents\ELI_NOSQL_Ant'
-
-$CP = 'build\classes;lib\java-cup-11b-runtime.jar;lib\java-cup-11b.jar'
-
-Start-Process -FilePath 'java' `
-    -ArgumentList '-cp', $CP, 'analizadores.Analizadores' `
-    -NoNewWindow
